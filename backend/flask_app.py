21b9cb13aca276fbeecd7c961539e86e9d9d7db3
Michael Morasch <mmorasch@posteo.de>
2023-09-13 18:56:37 +0200

    wip

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 # TODO: get a datapoint, including all columns
 @app.route("/datapoint", methods=["GET"])
 def get_datapoint():
-    return "<p>Hello, World!</p>"
+    return {"id": "1", "baseRent": "5"}
 
 # TODO: get experts opinion on datapoint given datapoint id
 @app.route("/expert/<datapoint_id>", methods=["GET"])
 def get_expert(datapoint_id):
-    return f'<p>Hello, World! {datapoint_id} </p>'
+    return {"result": "expert opinion"} # TODO 0 or 1?
 
 # TODO: get prediction from xai.py for given datapoint id
 @app.route("/prediction/<datapoint_id>", methods=["GET"])
 def get_prediction(datapoint_id):
-    return f'<p>Hello, World! {datapoint_id} </p>'
+    return {"result": "prediction"} # TODO 0 or 1?
 
 # TODO: langchain prompt OpenAI with chat message. require slug for tracking in backend
 @app.route("/message/<slug>", methods=["POST"])
