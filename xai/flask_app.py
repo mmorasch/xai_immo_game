328b21174bb3bd7e937aec9382185a4c72fabb4b
Michael Morasch <mmorasch@posteo.de>
2023-09-14 22:12:22 +0200

    wip

--- xai/flask_app.py
+++ xai/flask_app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 
 from experiment_manager import ExperimentManager
 
 app = Flask(__name__)
 
+CORS(app)
+
 manager = ExperimentManager()
+
 
 
 # TODO: get a datapoint, including all columns
@@ -13,12 +17,15 @@ def get_datapoint():
     next_instance_dict = manager.get_next_instance()
     return next_instance_dict
 
+# TODO: get threshold
+@app.route("/threshold", methods=["GET"])
+def get_datapoint():
+    return {"threshold": "200"}
 
 # TODO: get experts opinion on datapoint given datapoint id
 @app.route("/expert/<datapoint_id>", methods=["GET"])
 def get_expert(datapoint_id):
     return {"result": "expert opinion"}  # TODO 0 or 1?
-
 
 # TODO: get prediction from xai_explainer.py for given datapoint id
 @app.route("/prediction/<datapoint_id>", methods=["GET"])
@@ -27,12 +34,10 @@ def get_prediction(datapoint_id):
     prediction = manager.get_current_prediction()
     return {"result": prediction}
 
-
 # TODO: langchain prompt OpenAI with chat message. require slug for tracking in backend
 @app.route("/message/<slug>", methods=["POST"])
 def post_message(slug):
     return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-    # clean up storage file on restart
     app.run(debug=False, port=4455, host='0.0.0.0')
